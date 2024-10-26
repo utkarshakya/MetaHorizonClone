53d000a61f829b0ce6fcb6c4fc93e9e5b77a8a65
Utkarsh Shakya <subhashshakya136@gmail.com>
2024-10-26 18:52:58 +0530

    UPDATED NAVBAR & HERO SECTION IN index.html FILE

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,16 @@ module.exports = {
         'hero5': "url('./vitalFiles/images/hero5.jpg')",
         'citadel': "url('./vitalFiles/images/citadel.jpg')",
         'endless': "url('./vitalFiles/images/endless.jpg')",
-        'vr': "url('./vitalFiles/images/vr.png')"
-      }
+        'vr': "url('./vitalFiles/images/vr.png')",
+      },
+    },
+    screens: {
+      'sm': '425px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1440px',
+      '2xl': '1536px',
+      '3xl': '2560px',
     }
   },
   plugins: [],
